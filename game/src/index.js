1558c2b383cd19909e6b7bb6f4bc621eac8853c3
byaqub <yaqub.benjamin@gmail.com>
2017-11-18 19:32:32 +0100

    initial commit

---
+++ game/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import './css/App.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
